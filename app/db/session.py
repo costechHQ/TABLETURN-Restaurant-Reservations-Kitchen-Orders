3fccb63a027d7a6888102fb9221227039d843403
Onyedika Christopher <cosimon.tech@gmail.com>
2026-09-20 02:57:31 +0100

    feat: set up database configuration and session

--- app/db/session.py
+++ app/db/session.py
@@ -1,8 +1,8 @@
+from collections.abc import Generator
 from sqlmodel import Session
-
 from app.db.database import engine
 
 
-def get_session():
+def get_session() -> Generator[Session, None, None]:
     with Session(engine) as session:
         yield session
