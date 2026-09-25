1a845355111803cbc8e468a48725fff8a1c84b18
costechHQ <273554019+costechHQ@users.noreply.github.com>
2026-09-25 23:07:52 +0100

    fix: make email unique/indexed.

--- app/models/user.py
+++ app/models/user.py
@@ -9,13 +9,27 @@ class UserRole(str, Enum):
     WAITER = "waiter"
     KITCHEN = "kitchen"
     MANAGER = "manager"
+
+
 class User(SQLModel, table=True):
     __tablename__ = "users"
 
-    id: int | None = Field(default=None, primary_key=True)
-    email: str
+    id: int | None = Field(
+        default=None,
+        primary_key=True,
+    )
+
+    email: str = Field(
+        unique=True,
+        index=True,
+    )
+
     password_hash: str
-    role: UserRole = Field(default=UserRole.DINER)
+
+    role: UserRole = Field(
+        default=UserRole.DINER,
+    )
+
     created_at: datetime = Field(
-        default_factory=lambda:datetime.now(timezone.utc)
+        default_factory=lambda: datetime.now(timezone.utc)
     )
